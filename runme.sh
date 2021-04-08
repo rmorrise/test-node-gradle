e303cdc729747908c10448becd76d08bd6a03ac9
rmorrise <russell.morrisey@cscglobal.com>
2021-04-08 17:57:01 -0400

    Remove second

--- runme.sh
+++ runme.sh
@@ -1,12 +1,12 @@
 docker build -t nodegradle:test .
 
-#echo "This works v v v v v"
-#docker run \
-#  -v $(pwd)/:/usr/src/app \
-#  --rm \
-#  nodegradle:test npm install
+echo "This works v v v v v"
+docker run \
+  -v $(pwd)/:/usr/src/app \
+  --rm \
+  nodegradle:test npm install
 
-echo "This doesn't work v v v v v"
+echo "This hangs with no output on npmInstall task v v v v v"
 docker run \
   -v $(pwd)/:/usr/src/app \
   --rm \
